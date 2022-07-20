4d1c1eb11f307f69f1968557c35f2679bf8f0412
Jonathan Wilson <jonathandw414@gmail.com>
2022-07-20 15:02:14 +0100

    added co2 chart using sample data

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@ import "./App.css";
 import Page from "./components/Page";
 import React from 'react';
 
+const dataIn = [
+    {
+        "time": "2022-07-20T03:00:00",
+        "val": 18.2
+    },
+    {
+        "time": "2022-07-20T04:00:00",
+        "val": 18.9
+    },
+    {
+        "time": "2022-07-20T05:00:00",
+        "val": 19.4
+    }
+];
+
 function App() {
-    return <Page/>;
+    const axisData = dataIn.map((dataPoint) => [dataPoint.time, dataPoint.val]);
+    return <Page axisData={axisData}/>;
 }
 
 export default App;
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactECharts from 'echarts-for-react';
 
-const Page: React.FC = () => {
+const Page = ({axisData}) => {
   const option = {
     title: {
-      text: '堆叠区域图'
+      text: 'CO2 against time'
     },
     tooltip : {
       trigger: 'axis'
     },
     legend: {
-      data:['邮件营销','联盟广告','视频广告']
+      data:['CO2']
     },
     toolbox: {
       feature: {
@@ -25,9 +25,7 @@ const Page: React.FC = () => {
     },
     xAxis : [
       {
-        type : 'category',
-        boundaryGap : false,
-        data : ['周一','周二','周三','周四','周五','周六','周日']
+        type : 'time',
       }
     ],
     yAxis : [
@@ -37,25 +35,11 @@ const Page: React.FC = () => {
     ],
     series : [
       {
-        name:'邮件营销',
+        name:'CO2',
         type:'line',
-        stack: '总量',
-        areaStyle: {normal: {}},
-        data:[120, 132, 101, 134, 90, 230, 210]
-      },
-      {
-        name:'联盟广告',
-        type:'line',
-        stack: '总量',
-        areaStyle: {normal: {}},
-        data:[220, 182, 191, 234, 290, 330, 310]
-      },
-      {
-        name:'视频广告',
-        type:'line',
-        stack: '总量',
-        areaStyle: {normal: {}},
-        data:[150, 232, 201, 154, 190, 330, 410]
+        // stack: '总量',
+        // areaStyle: {normal: {}},
+        data: axisData
       }
     ]
   };
